061c6388eaf4f4824b3acfb54c66b417c9df7479
mosull20 <mosullivan@mac.com>
2021-11-16 12:06:22 +0000

    Create add_review template

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,14 @@
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
+
+  $(document).ready(function() {
+    $('textarea#synopsis_text, textarea#review_text').characterCounter();
+  });
+
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+       
        
