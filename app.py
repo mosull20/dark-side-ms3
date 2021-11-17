6b6f0cba2999425f1f293f19a3d7c56c2a96cac0
mosull20 <mosullivan@mac.com>
2021-11-17 16:57:59 +0000

    Add Delete button and wire up to db

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def add_review():
         }
         mongo.db.reviews.insert_one(review)
         flash("Your review has been successfully added")
-        return redirect(url_for("get_reviews"))
+        return redirect(url_for("profile", username=session["user"]))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_review.html", categories=categories)
@@ -137,6 +137,13 @@ def edit_review(review_id):
     review = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_review.html", review=review, categories=categories)
+
+
+@app.route("/delete_review/<review_id>")
+def delete_review(review_id):
+    mongo.db.reviews.remove({"_id": ObjectId(review_id)})
+    flash("Your review has been deleted")
+    return redirect(url_for("profile", username=session["user"]))
 
 
 if __name__ == "__main__":
